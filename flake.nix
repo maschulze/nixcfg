fc05b571c07e34cc35f2e41f9513ee421a9d1889
Maik <7000347+maschulze@users.noreply.github.com>
2024-12-06 08:18:42 +0100

    updated

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = { self, nixpkgs, disko, ... }:
+    let
+      # TODO: Adjust these values to your needs
+      system = "x86_64-linux";
+      hostName = "thinkpad";
+    in
+    {
+      nixosConfigurations.${hostName} = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./disk-config.nix
+          disko.nixosModules.disko
+          ({ pkgs, ... }: {
+            boot.loader = {
+              systemd-boot.enable = true;
+              efi.canTouchEfiVariables = true;
+            };
+            networking = { inherit hostName; };
+            services.openssh.enable = true;
+            environment.systemPackages = with pkgs; [
+              htop
+              git
+              neovim
+            ];
+
+            {
+              users.extraUsers.worker = {
+                isNormalUser = true;
+                uid = 1000;
+                extraGroups = [ "users" "wheel" ];
+                initialPassword = "start123";
+              };
+              nix.settings.trusted-users = ["worker"];
+            }
+            system.stateVersion = "24.11";
+          })
+        ];
+      };
+    };
+}
