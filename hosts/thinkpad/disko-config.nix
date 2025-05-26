568494c8bf546cb811611aa87364c0a9aca55653
Maik <7000347+maschulze@users.noreply.github.com>
2025-05-26 09:00:37 +0200

    Added some comments to the disko configuration

--- hosts/thinkpad/disko-config.nix
+++ hosts/thinkpad/disko-config.nix
@@ -1,31 +1,38 @@
 {...}: {
+  # Configure storage devices using disko
   disko.devices = {
     disk = {
       nixos = {
-        type = "disk";
-        device = "/dev/sda";
+        type = "disk"; # This defines a physical disk
+        device = "/dev/sda"; # The device path for the disk
+
         content = {
-          type = "gpt";
+          type = "gpt"; # Use a GPT partition table
           partitions = {
+            # BIOS Boot Partition, required for GRUB in BIOS mode with GPT
             boot = {
               size = "1M";
-              type = "EF02"; # for grub MBR
+              type = "EF02"; # Partition type EF02 is for BIOS boot (GRUB MBR)
             };
+
+            # EFI System Partition, required for UEFI boot
             ESP = {
-              size = "512M";
-              type = "EF00";
+              size = "512M"; # 512 MB for the EFI System Partition
+              type = "EF00"; # Partition type EF00 is for EFI System Partition
               content = {
                 type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
+                format = "vfat"; # EFI partitions must be FAT32/vfat
+                mountpoint = "/boot"; # Mount at /boot for bootloader and kernels
               };
             };
+
+            # Root partition for the main filesystem
             root = {
-              size = "100%";
+              size = "100%"; # Use the remaining disk space
               content = {
                 type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
+                format = "ext4"; # Use ext4 filesystem for root
+                mountpoint = "/"; # Mount at root
               };
             };
           };
