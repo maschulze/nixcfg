f0059e2db2bd8082a5f6cd089729eed8662a8838
Maik <7000347+maschulze@users.noreply.github.com>
2025-06-02 08:53:16 +0200

    Added virtualbox guest option

--- hosts/virtualbox/hardware-configuration.nix
+++ hosts/virtualbox/hardware-configuration.nix
@@ -34,5 +34,5 @@
   # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  virtualisation.virtualbox.guest.enable = true;
 }
