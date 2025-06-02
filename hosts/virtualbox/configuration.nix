dd50a18d9e3ef785cf5d399f9618bfd9c03f5ff6
Maik <7000347+maschulze@users.noreply.github.com>
2025-06-02 08:43:30 +0200

    Added support for VirtualBox

---
+++ hosts/virtualbox/configuration.nix
@@ -0,0 +1,147 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./disko-config.nix
+
+    inputs.disko.nixosModules.default
+  ];
+
+  myNixOS = {
+    # bundles.general-desktop.enable = true;
+    hyprland.enable = true;
+    power-management.enable = true;
+
+    # virtualisation.enable = lib.mkDefaut true;
+
+    bundles.users.enable = true;
+    home-users = {
+      "worker" = {
+        userConfig = ./home.nix;
+        userSettings = {
+          extraGroups = [
+            "networkmanager"
+            "wheel"
+          ];
+          # hashedPasswordFile = "/persist/passwd";
+        };
+      };
+    };
+
+    # impermanence.enable = true;
+    # impermanence.nukeRoot.enable = true;
+  };
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "virtualbox"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = false;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "de";
+    variant = "nodeadkeys";
+  };
+
+  # Configure console keymap
+  console.keyMap = "de-latin1-nodeadkeys";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #  wget
+    # vscode
+    # kitty
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  system.stateVersion = "25.05";
+}
---
+++ hosts/virtualbox/home.nix
@@ -0,0 +1,150 @@
+{
+  inputs,
+  outputs,
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  imports = [ outputs.homeManagerModules.default ];
+
+  #   myHomeManager.impermanence.data.directories = [
+  #     "nixconf"
+
+  #     "Videos"
+  #     "Documents"
+  #     "Projects"
+  #   ];
+
+  #   myHomeManager.impermanence.cache.directories = [
+  #     ".local/share/PrismLauncher"
+  #     ".config/openvr"
+  #     ".config/tidal-hifi"
+
+  #     "Android"
+  #     ".local/share/godot"
+  #     ".config/alvr"
+  #   ];
+
+  #   programs.foot.enable = true;
+  #   programs.wezterm.enable = true;
+
+  myHomeManager = {
+    #     bundles.general.enable = true;
+    #     bundles.desktop-full.enable = true;
+
+    #     bundles.gaming.enable = true;
+
+    git.enable = true;
+    kitty.enable = true;
+    vscode.enable = true;
+    rofi.enable = true;
+    htop.enable = true;
+
+    firefox.enable = true;
+
+    hyprland.enable = true;
+
+    #     pipewire.enable = true;
+    #     tenacity.enable = true;
+
+    hyprland.split-workspaces.enable = false;
+
+    monitors = {
+      "eDP-1" = {
+        width = 2560;
+        height = 1440;
+        refreshRate = 60.;
+      };
+    };
+
+    #     workspaces = {
+    #       "2" = {
+    #         monitorId = 0;
+    #         autostart = with pkgs; [
+    #          (lib.getExe firefox)
+    #         ];
+    #       };
+    #       "10" = {
+    #         monitorId = 1;
+    #         autostart =  with pkgs; [
+    #           (lib.getExe telegram-desktop)
+    #           # (lib.getExe vesktop)
+    #         ];
+    #       };
+    #     };
+  };
+
+  home = {
+    username = "worker";
+    homeDirectory = lib.mkDefault "/home/worker";
+    stateVersion = "25.05";
+
+    # packages = with pkgs; [
+    #   obs-studio
+    #   wf-recorder
+    #   prismlauncher
+    #   tidal-hifi
+
+    #   opencomposite
+    # ];
+  };
+
+  # xdg.configFile."openxr/1/active_runtime.json".text =
+  # ''
+  #   {
+  #       "file_format_version" : "1.0.0",
+  #       "runtime" :
+  #       {
+  #           "VALVE_runtime_is_steamvr" : true,
+  #           "library_path" : "/home/yurii/.local/share/Steam/steamapps/common/SteamVR/bin/linux64/vrclient.so",
+  #           "name" : "SteamVR"
+  #       }
+  #   }
+  # '';
+
+  # xdg.configFile."openvr/openvrpaths.vrpath".text = ''
+  #   {
+  #     "config" :
+  #     [
+  #       "${config.xdg.dataHome}/Steam/config"
+  #     ],
+  #     "external_drivers": [
+  #       "${pkgs.alvr}/lib64/alvr"
+  #     ],
+  #     "jsonid": "vrpathreg",
+  #     "log" :
+  #     [
+  #       "${config.xdg.dataHome}/Steam/logs"
+  #     ],
+  #     "runtime" :
+  #     [
+  #       "${pkgs.opencomposite}/lib/opencomposite"
+  #     ],
+  #     "version" : 1
+  #   }
+  # '';
+
+  #   xdg.configFile."openxr/1/active_runtime.json".source = "${pkgs.wivrn}/share/openxr/1/openxr_wivrn.json";
+
+  #   xdg.configFile."openvr/openvrpaths.vrpath".text = ''
+  #     {
+  #       "config" :
+  #       [
+  #         "${config.xdg.dataHome}/Steam/config"
+  #       ],
+  #       "external_drivers" : null,
+  #       "jsonid" : "vrpathreg",
+  #       "log" :
+  #       [
+  #         "${config.xdg.dataHome}/Steam/logs"
+  #       ],
+  #       "runtime" :
+  #       [
+  #         "${pkgs.opencomposite}/lib/opencomposite"
+  #       ],
+  #       "version" : 1
+  #     }
+  #   '';
+}
