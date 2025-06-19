94d1c21d9f408c67901765840229b1ab99f5cc98
maschulze <maschulze@users.noreply.github.com>
2025-06-19 22:51:35 +0200

    Update disko.nix

--- Solution_2/hosts/x86/thinkpad/disko.nix
+++ Solution_2/hosts/x86/thinkpad/disko.nix
@@ -3,39 +3,31 @@
 }:
 {
   # Configure storage devices using disko
+  {
   disko.devices = {
     disk = {
-      nixos = {
-        type = "disk"; # This defines a physical disk
-        device = "/dev/sda"; # The device path for the disk
-
+      my-disk = {
+        device = "/dev/sda";
+        type = "disk";
         content = {
-          type = "gpt"; # Use a GPT partition table
+          type = "gpt";
           partitions = {
-            # BIOS Boot Partition, required for GRUB in BIOS mode with GPT
-            boot = {
-              size = "1M";
-              type = "EF02"; # Partition type EF02 is for BIOS boot (GRUB MBR)
-            };
-
-            # EFI System Partition, required for UEFI boot
             ESP = {
-              size = "512M"; # 512 MB for the EFI System Partition
-              type = "EF00"; # Partition type EF00 is for EFI System Partition
+              type = "EF00";
+              size = "500M";
               content = {
                 type = "filesystem";
-                format = "vfat"; # EFI partitions must be FAT32/vfat
-                mountpoint = "/boot"; # Mount at /boot for bootloader and kernels
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
               };
             };
-
-            # Root partition for the main filesystem
             root = {
-              size = "100%"; # Use the remaining disk space
+              size = "100%";
               content = {
                 type = "filesystem";
-                format = "ext4"; # Use ext4 filesystem for root
-                mountpoint = "/"; # Mount at root
+                format = "ext4";
+                mountpoint = "/";
               };
             };
           };
@@ -43,4 +35,5 @@
       };
     };
   };
+};
 }
